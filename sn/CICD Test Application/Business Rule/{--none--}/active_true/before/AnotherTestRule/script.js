4b31bdc99afdea7395fd311a7f4f1bfa73430c0f
nejifresh <nejifresh@gmail.com>
2020-02-04 08:39:01 -0700

    'master' branch updated from  https://dev70700.service-now.com no-cicd

---
+++ sn/CICD Test Application/Business Rule/{--none--}/active_true/before/AnotherTestRule/script.js
@@ -0,0 +1,16 @@
+/* 
+ * Application : CICD Test Application
+ * ClassName   : sys_script
+ * Created On  : 2020-02-04 14:52:50
+ * Created By  : admin
+ * Updated On  : 2020-02-04 15:05:46
+ * Updated By  : admin
+ * URL         : /sys_script.do?sys_id=32247d05dbb24010df8cab7dca9619f4
+ */
+(function executeRule(current, previous /*null when async*/) {
+
+	// Add your code here
+	
+	print('This is the client version');
+
+})(current, previous);
---
+++ sn/CICD Test Application/Business Rule/{--none--}/active_true/before/testrule/script.js
@@ -0,0 +1,14 @@
+/* 
+ * Application : CICD Test Application
+ * ClassName   : sys_script
+ * Created On  : 2020-02-04 14:53:44
+ * Created By  : admin
+ * Updated On  : 2020-02-04 14:53:44
+ * Updated By  : admin
+ * URL         : /sys_script.do?sys_id=df647545dbb24010df8cab7dca96199d
+ */
+(function executeRule(current, previous /*null when async*/) {
+
+	// Add your code here
+
+})(current, previous);
