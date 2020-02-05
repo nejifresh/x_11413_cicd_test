8c645d7d794d56c0fbd51d2c8e648446b82a02cb
nejifresh <nejifresh@gmail.com>
2020-02-05 15:14:49 -0700

    'master' branch updated from  https://dev70700.service-now.com no-cicd

--- sn/CICD Test Application/Business Rule/{--none--}/active_true/before/AnotherTestRule/script.js
+++ sn/CICD Test Application/Business Rule/{--none--}/active_true/before/AnotherTestRule/script.js
@@ -3,7 +3,7 @@
  * ClassName   : sys_script
  * Created On  : 2020-02-04 14:52:50
  * Created By  : admin
- * Updated On  : 2020-02-05 19:30:35
+ * Updated On  : 2020-02-05 21:47:01
  * Updated By  : admin
  * URL         : /sys_script.do?sys_id=32247d05dbb24010df8cab7dca9619f4
  */
@@ -15,5 +15,6 @@
 	print('Code in here');
 	print('This function runs on execute');
 	print('This is extra code');
+	print('Issue not here');
 
 })(current, previous);
